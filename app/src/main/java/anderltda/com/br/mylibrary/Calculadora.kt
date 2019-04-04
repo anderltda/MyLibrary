a6049fcb26806b70c108e04713687dbde6a3de4a
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:02:03 -0300

    Initial commit

---
+++ app/src/main/java/anderltda/com/br/mylibrary/Calculadora.kt
@@ -0,0 +1,13 @@
+package anderltda.com.br.mylibrary
+
+class Calculadora {
+
+    fun somar(v1 : Int, v2 : Int) : Int {
+        return v1 + v2
+    }
+
+    fun subtrair(v1 : Int, v2 : Int) : Int {
+        return v1 - v2
+    }
+
+}
